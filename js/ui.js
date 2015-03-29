5a8c768523c6c196584e13b199f4ebf89d037de5
Nicholas Egan <nicholas@egansoft.com>
2015-03-28 23:44:23 -0700

    Fixed display issues, added resume, and cleaned things up

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,4 @@
+// Sticky
 $(function($){
     $(document).ready(function() {
         $('#navbar-wrap').stickUp({
@@ -10,95 +11,12 @@ $(function($){
                 5:'contact'
             },
             itemClass: 'nav-item',
-            itemHover: 'active'
+            itemHover: 'active',
+            stickCallback: function() {$('#arrow').hide();console.log('hi')},
+            unstickCallback: function() {$('#arrow').show();console.log('bye')}
         })
     })
 })
-
-/*
-;(function(Shine){
-    var shine = new Shine(document.getElementById('banner-name'), new shinejs.Config({
-        numSteps: 5,
-        opacity: 0.9,
-        opacityPow: 1.2,
-        offset: 0.25,
-        offsetPow: 3.2,
-        blur: 48,
-        blurPow: 1,
-        shadowRGB: new shinejs.Color(255,255,255)
-    }))
-
-    window.addEventListener('mousemove', function(event){
-        shine.light.position.x = event.clientX
-        shine.light.position.y = event.clientY
-        shine.draw()
-    }, false)
-
-})(Shine)
-*/
-
-/*
-particlesJS('particles', {
-    particles: {
-        color: '#686868',
-        color_random: true,
-        shape: 'circle', // "circle", "edge" or "triangle"
-        opacity: {
-            opacity: 1,
-            anim: {
-                enable: true,
-                speed: 1.5,
-                opacity_min: 0,
-                sync: false
-            }
-        },
-        size: 4,
-        size_random: true,
-        nb: 125,
-        line_linked: {
-            enable_auto: true,
-            distance: 50,
-            color_random: true,
-            opacity: 1,
-            width: 1,
-            condensed_mode: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 600
-            }
-        },
-        anim: {
-            enable: true,
-            speed: 1
-        }
-    },
-    interactivity: {
-        enable: true,
-        mouse: {
-            distance: 300
-        },
-        detect_on: 'window', // "canvas" or "window"
-        mode: 'grab', // "grab" of false
-        line_linked: {
-            opacity: .5
-        },
-        events: {
-            onclick: {
-                enable: true,
-                mode: 'push', // "push" or "remove"
-                nb: 1
-            },
-            onresize: {
-                enable: true,
-                mode: 'out', // "out" or "bounce"
-                density_auto: false,
-                density_area: 800 // nb_particles = particles.nb * (canvas width *	canvas height / 1000) / density_area
-            }
-        }
-    },
-    retina_detect: true
-})
-*/
 
 // Switchy
 ;(function($) {
